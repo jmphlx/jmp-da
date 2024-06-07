22fc48b38cd335097ed5e30a6339cc6e56200297
Laurel Timko <laurel.timko@gmail.com>
2024-06-07 15:28:14 -0400

    Updated jmp-all index and add limit to listgroup

--- blocks/listgroup/listgroup.js
+++ blocks/listgroup/listgroup.js
@@ -2,19 +2,23 @@ import { getJsonFromUrl, parseBlockOptions } from '../../scripts/aem.js';
 
 export default async function decorate(block) {
 
-  const pathval = block.firstElementChild?.children.item(1).textContent;
+  const optionsObject = parseBlockOptions(block);
   block.firstElementChild.remove();
-
-  console.log(pathval);
   const container = document.createElement('div');
   const wrapper = document.createElement('ul');
   wrapper.classList = 'listOfItems image-list list-tile';
 
   const url = '/jmp-all.json';
   const { data: allPages } = await getJsonFromUrl(url);
-  console.log(allPages);
 
-  allPages.forEach((item) => {
+  let pageSelection = allPages;
+
+  const limitObjects = optionsObject.limit;
+  if(limitObjects !== undefined) {
+    pageSelection = pageSelection.slice(0, limitObjects);
+  }
+
+  pageSelection.forEach((item) => {
     const listItem = document.createElement('li');
     const cardLink = document.createElement('a');
     cardLink.href = item.path;
@@ -22,9 +26,9 @@ export default async function decorate(block) {
     cardLink.innerHTML = `
     <span class="title">${item.title}</span>
     <span class="cmp-image image"><img src="${item.image}"/></span>
-    <span class="abstract">${item.description}</span>"
+    <span class="abstract">${item.description}</span>
   `;
-    listItem.append(cardLink)
+    listItem.append(cardLink);
     wrapper.append(listItem);
   });
 
