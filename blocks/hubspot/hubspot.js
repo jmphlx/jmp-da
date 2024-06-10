f49ec6d09908396588d04a514e334f05d345ca62
Drew Cummings <doc7191@gmail.com>
2024-06-10 05:35:14 -0400

    linting
    fixing linting issues

--- blocks/hubspot/hubspot.js
+++ blocks/hubspot/hubspot.js
@@ -6,31 +6,34 @@
 
 const embedHubspot = (fRegion, fPortalId, fFormId, sfdcCampaignId = null) => {
   // clean up hubspot url query paramaters
-  
+
   const scriptHubspot = document.createElement('script');
   scriptHubspot.setAttribute('type', 'text/javascript');
   scriptHubspot.src = 'https://js.hsforms.net/forms/embed/v2.js';
-  
-  if (sfdcCampaignId) { //check if form has a salesforce campaign id
+
+  if (sfdcCampaignId) {
+    // check if form has a salesforce campaign id
     const fsfdcCampaignIdVal = sfdcCampaignId.textContent;
     scriptHubspot.addEventListener('load', () => {
       hbspt.forms.create({
         region: fRegion,
         portalId: fPortalId,
         formId: fFormId,
-        sfdcCampaignIdVal: fsfdcCampaignIdVal
+        sfdcCampaignIdVal: fsfdcCampaignIdVal,
       });
-    });    
-  } else { //function to call if it doesn't
+    });
+  } else {
+    // function to call if it doesn't
     scriptHubspot.addEventListener('load', () => {
       hbspt.forms.create({
         region: fRegion,
         portalId: fPortalId,
-        formId: fFormId
+        formId: fFormId,
       });
-    });}
+    });
+  }
   document.head.append(scriptHubspot);
-  
+
   const embedHTML = `
   <script>
     hbspt.forms.create({});
@@ -44,30 +47,33 @@ const loadEmbed = (block, region, portalId, formID) => {
     return;
   }
 
-  block.innerHTML = embedHubspot(region,portalId,formID)
-  block.classList = `block embed embed-hubspot`;
-  
+  block.innerHTML = embedHubspot(region, portalId, formID);
+  block.classList = 'block embed embed-hubspot';
+
   block.classList.add('form-is-loaded');
 };
 
 export default function decorate(block) {
-  const [region, portalId, formID, sfdcCampaignId] = [...block.children].map((c) => c.firstElementChild); //mapping variables
-  
-  const regionVal = region.textContent; //extracting text content
+  const [region, portalId, formID, sfdcCampaignId] = [...block.children].map(
+    (c) => c.firstElementChild,
+  ); // mapping variables
+
+  const regionVal = region.textContent; // extracting text content
   const portalIdVal = portalId.textContent;
   const formIDVal = formID.textContent;
 
-  if (sfdcCampaignId){  const sfdcCampaignIdVal = sfdcCampaignId.textContent;
+  if (sfdcCampaignId) {
+    const sfdcCampaignIdVal = sfdcCampaignId.textContent;
   }
 
   block.textContent = '';
 
-  const observer = new IntersectionObserver((entries) => { //calling embed function
+  const observer = new IntersectionObserver((entries) => {
+    // calling embed function
     if (entries.some((e) => e.isIntersecting)) {
       observer.disconnect();
       loadEmbed(block, regionVal, portalIdVal, formIDVal, sfdcCampaignId);
     }
   });
   observer.observe(block);
-  
 }
