c9c4c98a529d61f6711e7b0d66d2d33cc9aa986d
Drew Cummings <doc7191@gmail.com>
2024-07-29 12:32:08 -0400

    formatting update
    modified block to support Tad's formatting request

--- blocks/hubspot/hubspot.js
+++ blocks/hubspot/hubspot.js
@@ -4,15 +4,11 @@
  */
 /*  global hbspt  */
 
-const embedHubspot = (
-  fRegion,
-  fPortalId,
-  fFormId,
-  sfdcCampaignId = null,
-  lastAction = null,
-  leadSource = null,
-) => {
+import { readBlockConfig } from "../../scripts/aem.js";
+
+const embedHubspot = (config) => {
   // clean up hubspot url query paramaters
+  const sfdcCampaignId = config['salesforce-campaign-id'];
 
   const head = document.querySelector('head');
   const script = document.createElement('script');
@@ -23,41 +19,28 @@ const embedHubspot = (
   scriptHubspot.setAttribute('type', 'text/javascript');
   scriptHubspot.src = 'https://js.hsforms.net/forms/embed/v2.js';
 
-  if (lastAction) {
-    // check if form has last action field
-    lastAction = lastAction.textContent;
-  }
-
-  if (leadSource) {
-    // check if form has lead source field
-    leadSource = leadSource.textContent;
-  }
-
-  if (sfdcCampaignId) {
-    // check if form has a salesforce campaign id
-    sfdcCampaignId = sfdcCampaignId.textContent;
-  }
-
   // adds event listener to add embed code on load
   scriptHubspot.addEventListener('load', () => {
     hbspt.forms.create({
-      region: fRegion,
-      portalId: fPortalId,
-      formId: fFormId,
+      region: config['region'],
+      portalId: config['portal-id'],
+      formId: config['form-id'],
       sfdcCampaignId,
       onFormReady($form) {
-        const hiddenField = $form.find('input[name="last_action__c"]');
-        const newValue = lastAction;
-        hiddenField2.val(newValue).change();
+        
 
-        const hiddenField2 = $form.find('input[name="leadsource"]');
-        const newValue2 = leadSource;
-        hiddenField.val(newValue2).change();
+        var hiddenField2 = $form.find('input[name="last_action"]');
+        var newValue2 = config['last-action']; // The value you want to append
+        hiddenField2.val(newValue2).change(); 
+
+        var hiddenField = $form.find('input[name="leadsource"]');
+        var newValue = config['lead-source']; // The value you want to append
+        hiddenField.val(newValue).change(); 
 
         const emailSFC = $form.find('input[name="salesforce_campaign_event_id"]');
         const newSFC = sfdcCampaignId;
         emailSFC.val(newSFC).change();
-      },
+ }
     });
   });
 
@@ -71,25 +54,22 @@ const embedHubspot = (
   return embedHTML;
 };
 
-const loadEmbed = (block, region, portalId, formID, sfdcCampaignId, lastAction, leadSource) => {
+const loadEmbed = (block, config) => {
   if (block.classList.contains('form-is-loaded')) {
     return;
   }
 
-  block.innerHTML = embedHubspot(region, portalId, formID, sfdcCampaignId, lastAction, leadSource);
+  block.innerHTML = embedHubspot(config);
   block.classList = 'block embed embed-hubspot';
 
   block.classList.add('form-is-loaded');
 };
 
 export default function decorate(block) {
-  const [region, portalId, formID, sfdcCampaignId, lastAction, leadSource] = [
-    ...block.children,
-  ].map((c) => c.firstElementChild); // mapping variables
 
-  const regionVal = region.textContent; // extracting text content
-  const portalIdVal = portalId.textContent;
-  const formIDVal = formID.textContent;
+  const config = readBlockConfig(block);
+
+  console.log(config['last-action']);
 
   block.textContent = '';
 
@@ -97,7 +77,7 @@ export default function decorate(block) {
     // calling embed function
     if (entries.some((e) => e.isIntersecting)) {
       observer.disconnect();
-      loadEmbed(block, regionVal, portalIdVal, formIDVal, sfdcCampaignId, lastAction, leadSource);
+      loadEmbed(block, config);
     }
   });
   observer.observe(block);
