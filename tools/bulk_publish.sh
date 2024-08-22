027526a8c86151520105c94c688ed34c74be5249
Dwayne Hale <143008962+dwayne-hale@users.noreply.github.com>
2024-08-22 11:54:57 -0400

    Update bulk_publish.sh

--- tools/bulk_publish.sh
+++ tools/bulk_publish.sh
@@ -7,6 +7,6 @@ REPO="jmp-da"
 REF="main"
 #for PATH in ${PREVIEW_LIST}; do
 while read -r path; do
-    echo "${ACTIVATE_MODE} request sent for ${path}"
-    curl https://admin.hlx.page/${ACTIVATE_MODE}/${OWNER}/${REPO}/${REF}/${path} -H "authorization: token $API_KEY"
+    RESPONSE_CODE=`curl https://admin.hlx.page/${ACTIVATE_MODE}/${OWNER}/${REPO}/${REF}/${path} -w "%{http_code}" -H "authorization: token $API_KEY"`
+    echo "${ACTIVATE_MODE} request sent for ${path}, response code: ${RESPONSE_CODE}"
 done < "${PREVIEW_LIST}"
