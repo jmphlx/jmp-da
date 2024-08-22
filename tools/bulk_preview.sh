0752793b92e954fe7aefb87e4c846c3585ffddc9
Dwayne Hale <dwayne.hale@zaxbys.com>
2024-08-22 09:56:18 -0400

    bulk_preview script

--- tools/bulk_preview.sh
+++ tools/bulk_preview.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+ACTIVATE_MODE="preview" #could also be 'live' for publishing
 PREVIEW_LIST=""
 API_KEY=""
 OWNER="jmp-hlx"
 REPO="jmp-da"
 REF="main"
-for PATH in ${PREVIEW_LIST}; do
-    echo "Preview request sent for ${PATH}"
-    curl https://admin.hlx.page/preview/${OWNER}/${REPO}/${REF}/{path} -H "authorization: token $API_KEY"
-done
+#for PATH in ${PREVIEW_LIST}; do
+while read -r path; do
+    echo "Preview request sent for ${path}"
+    curl https://admin.hlx.page/${ACTIVATE_MODE}/${OWNER}/${REPO}/${REF}/${path} -H "authorization: token $API_KEY"
+done < "${PREVIEW_LIST}"
