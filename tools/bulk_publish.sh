3825bde58c42e4a5299ecbebf7654bc0e152fde8
Dwayne Hale <143008962+dwayne-hale@users.noreply.github.com>
2024-08-22 11:55:50 -0400

    Update bulk_publish.sh

--- tools/bulk_publish.sh
+++ tools/bulk_publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ACTIVATE_MODE="preview" #could also be 'live' for publishing
-PREVIEW_LIST=""
+PAGE_LIST=""
 API_KEY=""
 OWNER="jmp-hlx"
 REPO="jmp-da"
@@ -9,4 +9,4 @@ REF="main"
 while read -r path; do
     RESPONSE_CODE=`curl https://admin.hlx.page/${ACTIVATE_MODE}/${OWNER}/${REPO}/${REF}/${path} -w "%{http_code}" -H "authorization: token $API_KEY"`
     echo "${ACTIVATE_MODE} request sent for ${path}, response code: ${RESPONSE_CODE}"
-done < "${PREVIEW_LIST}"
+done < "${PAGE_LIST}"
