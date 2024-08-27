162336b9af686c4c254eec18ecbb1824df89466c
Laurel Timko <laurel.timko@jmp.com>
2024-08-27 16:00:02 -0400

    Back To Top Button

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,6 +1,28 @@
 import { getMetadata } from '../../scripts/aem.js';
 import { loadFragment } from '../fragment/fragment.js';
 import { getLanguageFooter } from '../../scripts/jmp.js';
+import { createTag } from '../../scripts/helper.js';
+
+function addBackToTopButton() {
+  const buttonNav = createTag('nav', { id: 'back-top' });
+  const buttonLink = createTag('a', { href: '#content' });
+  buttonLink.textContent = 'Back to Top';
+  buttonNav.append(buttonLink);
+
+  document.addEventListener('scroll', () => {
+    const scrollTop = (window.scrollY !== undefined) ? window.scrollY
+      : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+    if (scrollTop > 500) {
+      buttonNav.classList.add('is-scrolled');
+    } else {
+      buttonNav.classList.remove('is-scrolled');
+    }
+  });
+  buttonLink.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
+  return buttonNav;
+}
 
 /**
  * loads and decorates the footer
@@ -18,6 +40,9 @@ export default async function decorate(block) {
   // decorate footer DOM
   const footer = document.createElement('div');
   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
+
+  const backToTopButton = addBackToTopButton();
+  footer.append(backToTopButton);
 
   block.append(footer);
 }
