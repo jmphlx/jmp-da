a41c6d3a816ee18ad6b8a43adecf4f052b7fca97
Laurel Timko <laurel.timko@jmp.com>
2024-09-13 14:49:08 -0400

    Add TrustArc tags and script to delayed js

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,7 +1,22 @@
 // eslint-disable-next-line import/no-cycle
 import { sampleRUM } from './aem.js';
+import { createTag } from './helper.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
 // add more delayed functionality here
+// Add TrustArc for Cookie Management.
+function addTrustArc() {
+  const divConsentBlackBar = createTag('div', { id: 'consent_blackbar'});
+  const divTEConsent = createTag('div', { id: 'teconsent'});
+  const trustArcScript = createTag('script', { 
+    async: 'async',
+    type: 'text/javascript',
+    crossorigin: '',
+    src: '//consent.trustarc.com/notice?domain=jmp.com&c=teconsent&js=nj&noticeType=bb&gtm=1&text=true'
+  });
+  document.querySelector('footer').append(divConsentBlackBar, divTEConsent, trustArcScript);
+}
+
+addTrustArc();
