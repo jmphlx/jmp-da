27ab55ec46a874a235ede5bb09ddac352b759d18
Laurel Timko <laurel.timko@jmp.com>
2024-10-22 12:27:46 -0400

    Working custom listgroup with filters, tabs, and group by

--- blocks/listgroup-custom/listgroup-custom.js
+++ blocks/listgroup-custom/listgroup-custom.js
@@ -22,7 +22,12 @@ const propertyNames = {
   columns: 'columns',
   groupBy: 'group-by',
   filterBy: 'filter-by',
+  tabProperty: 'tab-property',
+  tabs: 'tabs',
 };
+
+let useFilter = false;
+let useTabs = false;
 
 function lowercaseObj(obj) {
   const newObj = {};
@@ -80,9 +85,13 @@ function pageMatches(page, filters) {
   return false;
 }
 
-function writeAsOneGroup(wrapper, matching, config) {
+function writeAsOneGroup(matching, config) {
+  const wrapper = document.createElement('ul');
   const columns = config[propertyNames.columns] ? config[propertyNames.columns] : 5;
   wrapper.classList = `listOfItems image-list list-tile col-size-${columns}`;
+  if (useTabs) {
+    wrapper.setAttribute('role', 'tabpanel');
+  }
   matching.forEach((item) => {
     const listItem = document.createElement('li');
     listItem.classList = `${item.resourceOptions}`;
@@ -115,7 +124,8 @@ function writeAsOneGroup(wrapper, matching, config) {
   return wrapper;
 }
 
-function writeAsAZGroups(wrapper, matching, groupBy, groupProperty, blockObj) {
+function writeAsAZGroups(matching, groupBy, groupProperty, blockObj) {
+  const wrapper = createTag('ul', { class: 'groupList' });
   const dictionary = matching.reduce((groups, page) => {
     const startingLetter = page[groupProperty][0].toLowerCase();
     // either push to an existing letter entry or create one
@@ -130,8 +140,7 @@ function writeAsAZGroups(wrapper, matching, groupBy, groupProperty, blockObj) {
     const heading = document.createElement('h3');
     heading.id = letter;
     heading.textContent = letter.toUpperCase();
-    const groupWrapper = document.createElement('ul');
-    writeAsOneGroup(groupWrapper, dictionary[letter], blockObj);
+    const groupWrapper = writeAsOneGroup(dictionary[letter], blockObj);
     letterGroup.append(heading, groupWrapper);
     wrapper.append(letterGroup);
   });
@@ -147,12 +156,16 @@ function writeAsAZGroups(wrapper, matching, groupBy, groupProperty, blockObj) {
     groupHeader.append(letterLink);
   });
   wrapper.prepend(groupHeader);
+
+  return wrapper;
 }
 
 function constructDictionary(matching, filterBy) {
   const dictionary = matching.reduce((filterGroups, page) => {
-    const filterValue = page[filterBy];
-    if (filterValue.indexOf(',') > 0) {
+    const lcPage = lowercaseObj(page);
+    const filterValue = lcPage[filterBy];
+    // is filterValue a list of values.
+    if (filterValue && filterValue.indexOf(',') > 0) {
       const filterValueArray = filterValue.split(',');
       filterValueArray.forEach((val) => {
         // either push to an existing entry or create one
@@ -161,6 +174,7 @@ function constructDictionary(matching, filterBy) {
       });
     } else {
       // either push to an existing entry or create one
+      // eslint-disable-next-line no-lonely-if
       if (filterGroups[filterValue]) filterGroups[filterValue].push(page);
       else filterGroups[filterValue] = [page];
     }
@@ -170,10 +184,13 @@ function constructDictionary(matching, filterBy) {
   return dictionary;
 }
 
-function constructDropdown(dictionary) {
-  const filterDropdown = createTag('select', { class: 'filterDropdown'});
+function constructDropdown(dictionary, filterBy) {
+  const filterDropdown = createTag('select', {
+    class: 'filterDropdown',
+    id: `${filterBy}`,
+  });
 
-  const allDropdownItem = createTag('option', { value: ''});
+  const allDropdownItem = createTag('option', { value: '' });
   allDropdownItem.textContent = 'All';
   filterDropdown.append(allDropdownItem);
 
@@ -188,6 +205,96 @@ function constructDropdown(dictionary) {
   return filterDropdown;
 }
 
+function applyFilter(matching, filterBy, filterValue) {
+  const filteredData = matching.filter((item) => {
+    // Need to check if it is an array and contains
+    const conditionObject = {
+      property: filterBy,
+      value: filterValue,
+    };
+    return containsOperator(item, conditionObject);
+  });
+  return filteredData;
+}
+
+function buildListItems(block, matching, tabDictionary, config) {
+  let pageSelection = matching;
+  if (useTabs) {
+    const selectedTab = block.querySelector('button[aria-selected="true"].tabs-tab');
+    if (selectedTab.id !== 'all') {
+      pageSelection = tabDictionary[selectedTab.id];
+    }
+  }
+
+  if (useFilter) {
+    const filterDropdown = block.querySelector('select');
+    if (filterDropdown.value) {
+      pageSelection = applyFilter(pageSelection, filterDropdown.id, filterDropdown.value);
+    }
+  }
+
+  const limit = config[propertyNames.limit];
+  if (limit !== undefined && pageSelection.length > limit) {
+    pageSelection = pageSelection.slice(0, limit);
+  }
+
+  const emptyResultsMessage = config[propertyNames.emptyResultsMessage];
+  if (pageSelection.length === 0 && emptyResultsMessage !== undefined) {
+    const emptyResultsDiv = document.createElement('div');
+    emptyResultsDiv.classList = 'listOfItems no-results';
+    emptyResultsDiv.innerHTML = `<span>${emptyResultsMessage}</span>`;
+    return emptyResultsDiv;
+  }
+
+  return writeAsOneGroup(pageSelection, config);
+}
+
+function reBuildList(matching, block, tabDictionary, config) {
+  block.querySelector('.listOfItems').remove();
+  block.append(buildListItems(block, matching, tabDictionary, config));
+}
+
+function constructTabsObject(tabsArray) {
+  const tabObj = {};
+
+  tabsArray.forEach((item) => {
+    if (item.includes('=')) {
+      const optionsString = item.split('=', 2);
+      tabObj[optionsString[0]] = optionsString[1];
+    }
+  });
+  return tabObj;
+}
+
+function constructTabs(matching, dictionary, tabs, block, config) {
+  const tablist = document.createElement('div');
+  tablist.className = 'tabs-list';
+  tablist.setAttribute('role', 'tablist');
+
+  // decorate tabs and tabpanels
+  Object.keys(tabs).forEach((tab, i) => {
+    // build tab button
+    const button = document.createElement('button');
+    button.className = 'tabs-tab';
+    button.id = `${tabs[tab]}`;
+    button.innerHTML = tab;
+    button.setAttribute('aria-controls', `tabpanel-${i}`);
+    button.setAttribute('aria-selected', !i);
+    button.setAttribute('role', 'tab');
+    button.setAttribute('type', 'button');
+    button.addEventListener('click', () => {
+      tablist.querySelectorAll('button').forEach((btn) => {
+        btn.setAttribute('aria-selected', false);
+      });
+      button.setAttribute('aria-selected', true);
+      reBuildList(matching, block, dictionary, config);
+    });
+    tablist.append(button);
+  });
+
+  return tablist;
+}
+
 export default async function decorate(block) {
   const config = getBlockConfig(block);
   block.textContent = '';
@@ -197,13 +304,16 @@ export default async function decorate(block) {
 
   const { data: allPages } = await getJsonFromUrl(languageIndexUrl);
   const filters = await getJsonFromUrl(config[propertyNames.filter]);
-  const limit = config[propertyNames.limit];
   const sortBy = config[propertyNames.sortBy]?.toLowerCase();
   const sortOrder = config[propertyNames.sortOrder]?.toLowerCase();
-  const emptyResultsMessage = config[propertyNames.emptyResultsMessage];
-  const groupBy = config[propertyNames.groupBy]?.toLowerCase();
-  const filterBy = config[propertyNames.filterBy]?.toLowerCase();
+  const groupBy = config[propertyNames.groupBy];
+  const filterBy = config[propertyNames.filterBy];
+  const tabProperty = config[propertyNames.tabProperty]?.toLowerCase();
+  const tabsArray = config[propertyNames.tabs];
+  useTabs = tabProperty && tabsArray;
+  useFilter = filterBy;
 
+  let wrapper;
   let matching = [];
   allPages.forEach((page) => {
     if (pageMatches(page, filters)) {
@@ -213,44 +323,34 @@ export default async function decorate(block) {
 
   matching = sortPageList(matching, sortBy, sortOrder);
 
-  // Apply limit to results.
-  // TO-DO: May need to move this to write methods
-  if (limit !== undefined && matching.length > limit) {
-    matching = matching.slice(0, limit);
-  }
+  const tabDictionary = useTabs ? constructDictionary(matching, tabProperty) : undefined;
+  const filterDictionary = useFilter ? constructDictionary(matching, filterBy) : undefined;
 
-  const wrapper = document.createElement('ul');
+  // Create Filter Dropdown
+  if (filterDictionary) {
+    const filterDropdown = constructDropdown(filterDictionary, filterBy);
 
-  if (filterBy) {
-    const dictionary = constructDictionary(matching, filterBy);
-    const filterDropdown = constructDropdown(dictionary);
-
-      // When value changes, clear out results and add matching values.
-    filterDropdown.addEventListener('change', (e) => {
-      wrapper.querySelectorAll('li').forEach(e => e.remove());
-      if (filterDropdown.value) {
-        writeAsOneGroup(wrapper, dictionary[filterDropdown.value], config);
-      } else {
-        writeAsOneGroup(wrapper, matching, config);
-      }
+    // When value changes, clear out results and add matching values.
+    filterDropdown.addEventListener('change', () => {
+      reBuildList(matching, block, tabDictionary, config);
     });
+
     block.append(filterDropdown);
   }
 
-  if (matching.length > 0) {
-    if (groupBy) {
-      wrapper.classList = 'groupList';
-      writeAsAZGroups(wrapper, matching, groupBy, sortBy, config);
-    } else {
-      writeAsOneGroup(wrapper, matching, config);
-    }
+  //  Create tab group
+  if (tabDictionary) {
+    const tabsObject = constructTabsObject(tabsArray);
+    const tabs = constructTabs(matching, tabDictionary, tabsObject, block, config);
+    block.append(tabs);
   }
 
-  if (matching.length === 0 && emptyResultsMessage !== undefined) {
-    const emptyResultsDiv = document.createElement('div');
-    emptyResultsDiv.classList = 'no-results';
-    emptyResultsDiv.innerHTML = `<span>${emptyResultsMessage}</span>`;
-    wrapper.append(emptyResultsDiv);
+  // Build initial list.
+  // Group by does not work with filter or tabs.
+  if (groupBy && matching.length > 0) {
+    wrapper = writeAsAZGroups(matching, groupBy, sortBy, config);
+  } else {
+    wrapper = buildListItems(block, matching, tabDictionary, config);
   }
   block.append(wrapper);
 }
