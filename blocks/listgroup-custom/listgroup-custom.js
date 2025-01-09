4910f8d056c2c9c1089c7b55c3f56a936ba12310
Laurel Timko <laurel.timko@jmp.com>
2025-01-09 10:13:07 -0500

    Fix error in custom listgroup

--- blocks/listgroup-custom/listgroup-custom.js
+++ blocks/listgroup-custom/listgroup-custom.js
@@ -85,7 +85,7 @@ function writeAsOneGroup(matching, config) {
     const listItem = document.createElement('li');
     listItem.classList = `${item.resourceOptions}`;
     const cardLink = document.createElement('a');
-    if (item.redirectTarget.length > 0) {
+    if (item.redirectTarget?.length > 0) {
       cardLink.href = item.redirectTarget;
       cardLink.target = '_blank';
     } else {
