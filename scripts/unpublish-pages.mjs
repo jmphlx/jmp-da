8696112073fca2890350b086d6404740e6defc04
Laurel Timko <laurel.timko@jmp.com>
2025-02-03 20:40:45 -0500

    Remove imports all together

--- scripts/unpublish-pages.mjs
+++ scripts/unpublish-pages.mjs
@@ -1,4 +1,37 @@
-import pkg from '../scripts/jmp.js';
+const languageIndexes = [
+  'en', 'es', 'fr', 'zh', 'de', 'it', 'ko', 'ja', 'zh-hans', 'zh-hant',
+];
+const baseURL = 'https://main--jmp-da--jmphlx.hlx.live';
+
+function getAllLanguageIndexes(includeFullURL) {
+  const indexPaths = [];
+  languageIndexes.forEach((currLang) => {
+    if (includeFullURL) {
+      indexPaths.push(`${baseURL}/jmp-${currLang}.json`);
+    } else {
+      indexPaths.push(`/jmp-${currLang}.json`);
+    }
+  });
+  return indexPaths;
+}
+
+/**
+ * Returns a list of properties listed in the block
+ * @param {string} route get the Json data from the route
+ * @returns {Object} the json data object
+*/
+async function getJsonFromUrl(route) {
+  try {
+    const response = await window.fetch(route);
+    if (!response.ok) return null;
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('getJsonFromUrl:', { error });
+  }
+  return null;
+}
 
 /**
  * Given a list of pages, filter down to event pages where the date has passed
@@ -8,7 +41,7 @@ import pkg from '../scripts/jmp.js';
  */
 async function getPastEventsPages(languageIndexUrl) {
   console.log(`index ${languageIndexUrl}`);
-  const { data: allPages } = await pkg.getJsonFromUrl(languageIndexUrl);
+  const { data: allPages } = await getJsonFromUrl(languageIndexUrl);
   const filteredPages = allPages.filter((item) => {
     if (item.offDateTime) {
       return new Date(item.offDateTime) <= new Date();
@@ -22,7 +55,7 @@ export default async function printStuff(printVar) {
   console.log('stuff');
   console.log(printVar);
   //const pagesToUnpublish = [];
-  const languageIndexes = pkg.getAllLanguageIndexes(true);
+  const languageIndexes = getAllLanguageIndexes(true);
   console.log(languageIndexes);
 
   const foundPages = getPastEventsPages('https://main--jmp-da--jmphlx.hlx.live/jmp-en.json');
