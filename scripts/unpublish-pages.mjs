9eb3714581bc55d619fefe5eaa9f51099c0a3a61
Laurel Timko <laurel.timko@jmp.com>
2025-02-07 14:47:24 -0500

    Wrong delimiter

--- scripts/unpublish-pages.mjs
+++ scripts/unpublish-pages.mjs
@@ -112,7 +112,7 @@ export default async function unpublishPastEvents(authToken) {
     }
   }
 
-  const textResponse = `${failedPages.length}-Successfully unpublished ${successPages} `
+  const textResponse = `${failedPages.length}.Successfully unpublished ${successPages} `
     + `Failed to unpublish  ${failedPages}`;
   return textResponse;
 }
