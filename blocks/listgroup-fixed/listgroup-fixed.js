ae84742985063f92a01cfd6f4fb325887344278e
Laurel Timko <laurel.timko@jmp.com>
2025-08-21 15:05:58 -0400

    Trim tag before checks

--- blocks/listgroup-fixed/listgroup-fixed.js
+++ blocks/listgroup-fixed/listgroup-fixed.js
@@ -34,6 +34,7 @@ function writeOutTagProperties(prop, doc, propValue) {
   const tagsValue = tagsProperty.split(',');
   const tagsArray = [];
   tagsValue.forEach((tag) => {
+    tag = tag.trim();
     if (tag.trim().startsWith(convertedProp)) {
       // Found the prop string. Convert it to displayable format
       if (window.tagtranslations[tag]) {
