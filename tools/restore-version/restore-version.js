b6c515087c44dfbe5f488015af8c0086423960d6
Laurel Timko <laurel.timko@jmp.com>
2025-11-03 14:30:18 -0500

    Linting clean up

--- tools/restore-version/restore-version.js
+++ tools/restore-version/restore-version.js
@@ -1,13 +1,36 @@
 // eslint-disable-next-line import/no-unresolved
 import DA_SDK from 'https://da.live/nx/utils/sdk.js';
+// eslint-disable-next-line import/no-unresolved
+import { crawl } from 'https://da.live/nx/public/utils/tree.js';
 import { formatVersions } from './formatter.js';
 import { createTag, saveToDa } from '../../scripts/helper.js';
 import { constructPageViewer } from '../search/ui.js';
-import { crawl } from 'https://da.live/nx/public/utils/tree.js';
 
 let context;
 let actions;
 let token;
+
+async function getVersionFromList(versionUrl) {
+  const url = `https://admin.da.live${versionUrl}`;
+  try {
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (response.ok) {
+      const result = await response.text();
+      return result;
+    }
+    const errorText = await response.text();
+    return { success: false, status: response.status, error: errorText };
+  } catch (e) {
+    console.log(e);
+    return { success: false, status: null, error: e };
+  }
+}
 
 function addLoadingSearch(container, loadingText) {
   container.innerHTML = '';
@@ -18,7 +41,7 @@ function addLoadingSearch(container, loadingText) {
 }
 
 function createInvalidCard(pagePath) {
-  const body = createTag('div', { class: 'card-body invalid'});
+  const body = createTag('div', { class: 'card-body invalid' });
   const emptyMessage = createTag('span', {
     class: 'invalid-msg',
   }, `No Versions Exist for ${pagePath}`);
@@ -27,7 +50,7 @@ function createInvalidCard(pagePath) {
 }
 
 function createCard(pagePath, versionList) {
-  const body = createTag('div', { class: 'card-body'});
+  const body = createTag('div', { class: 'card-body' });
 
   const checkbox = createTag('input', {
     name: 'selectRestore',
@@ -43,11 +66,11 @@ function createCard(pagePath, versionList) {
     }
   });
 
-  const cardPath = createTag('span', { class: 'title'});
+  const cardPath = createTag('span', { class: 'title' });
   cardPath.textContent = pagePath;
   body.append(checkbox, cardPath);
 
-  const versionContainer = createTag('div', { class: 'version-lists'});
+  const versionContainer = createTag('div', { class: 'version-lists' });
   if (versionList !== undefined && versionList.data.length > 0) {
     const versionDropdown = createTag('select');
 
@@ -56,15 +79,8 @@ function createCard(pagePath, versionList) {
         value: version.url,
       });
       versionOpt.textContent = `${version.date} ${version.time}`;
-      versionDropdown.append(versionOpt)
+      versionDropdown.append(versionOpt);
     });
-
-    // Should be a dropdown list of version. Need a button to compare the versions. 
-    // May need a button to create version of current state.
-    // maybe needs to link to open the page in a new window.
-    // may need a button to refresh version list OR have creating version prepend?
-    // future, add checkbox to each card, and then add header to the top of results
-    // header should have number selected, button to restore all selected version, button to select or unselect all.
 
     const openPageIcon = createTag('img', {
       src: `${window.location.origin}/icons/new-tab-icon.svg`,
@@ -99,30 +115,7 @@ function createCard(pagePath, versionList) {
   return body;
 }
 
-async function getVersionFromList(versionUrl) {
-  const url = `https://admin.da.live${versionUrl}`;
-  try {
-    const response = await fetch(url, {
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer ${token}`,
-      }
-    });
-
-    if (response.ok) {
-      const result = await response.text();
-      return result;
-    } else {
-      const errorText = await response.text();
-      return { success: false, status: response.status, error: errorText };
-    }
-  } catch (e) {
-    console.log(e);
-    return { success: false, status: null, error: e };
-  }
-}
-
-async function getVersionList(path, token) {
+async function getVersionList(path) {
   const url = `https://admin.da.live/versionlist${path}`;
   try {
     const response = await fetch(url, {
@@ -130,7 +123,7 @@ async function getVersionList(path, token) {
       headers: {
         Authorization: `Bearer ${token}`,
         'Content-Type': 'application/json',
-      }
+      },
     });
 
     if (response.ok) {
@@ -139,7 +132,7 @@ async function getVersionList(path, token) {
         try {
           const jsonResult = await response.json();
           const formattedList = formatVersions(jsonResult);
-          return { success: true, status: response.status, data: formattedList};
+          return { success: true, status: response.status, data: formattedList };
         } catch (jsonError) {
           console.log(jsonError);
           return { success: false, status: response.status, error: jsonError };
@@ -165,53 +158,51 @@ async function restoreVersion(pagePath, versionUrl) {
 }
 
 function createResultHeader() {
-      const resultHeader = createTag('div', { class: 'results-header' });
-    const resultHeaderText = createTag('h2', {}, 'Results');
-    const buttonContainer = createTag('div', { class: 'btn-container'});
-    const selectAllButton = createTag('button', {
-      type: 'button',
-      class: 'select-all-btn',
-    }, 'Select All');
+  const resultHeader = createTag('div', { class: 'results-header' });
+  const resultHeaderText = createTag('h2', {}, 'Results');
+  const buttonContainer = createTag('div', { class: 'btn-container' });
 
-    selectAllButton.addEventListener('click', () => {
-      const allCheckboxes = document.querySelectorAll('div.card-body input')
-      allCheckboxes.forEach((checkbox) => {
-        checkbox.checked = true;
-        checkbox.dispatchEvent(new Event('change', { bubbles: true }));
-      })
+  const selectAllButton = createTag('button', {
+    type: 'button',
+    class: 'select-all-btn',
+  }, 'Select All');
+  selectAllButton.addEventListener('click', () => {
+    const allCheckboxes = document.querySelectorAll('div.card-body input');
+    allCheckboxes.forEach((checkbox) => {
+      checkbox.checked = true;
+      checkbox.dispatchEvent(new Event('change', { bubbles: true }));
     });
+  });
 
-    const deselectAllButton = createTag('button', {
-      type: 'button',
-      class: 'deselect-all-btn',
-    }, 'Deselect All');
-
-    deselectAllButton.addEventListener('click', () => {
-      const allCheckboxes = document.querySelectorAll('div.card-body input')
-      allCheckboxes.forEach((checkbox) => {
-        checkbox.checked = false;
-        checkbox.dispatchEvent(new Event('change', { bubbles: true }));
-      })
+  const deselectAllButton = createTag('button', {
+    type: 'button',
+    class: 'deselect-all-btn',
+  }, 'Deselect All');
+  deselectAllButton.addEventListener('click', () => {
+    const allCheckboxes = document.querySelectorAll('div.card-body input');
+    allCheckboxes.forEach((checkbox) => {
+      checkbox.checked = false;
+      checkbox.dispatchEvent(new Event('change', { bubbles: true }));
     });
+  });
 
-    const restorePagesButton = createTag('button', {
-      type: 'button',
-      class: 'restore-btn',
-    }, 'Restore Page Versions');
-    
-    restorePagesButton.addEventListener('click', async function() {
-      const selectedCards = document.querySelectorAll('div.card-body input[type="checkbox"]:checked');
-      for (var i = 0; i < selectedCards.length; i++) {
-        const card = selectedCards[i].parentElement;
-        const versionUrl = card.querySelector('div.version-lists select')?.value;
-        await restoreVersion(selectedCards[i].value, versionUrl);
-        card.classList.add('modified');
-      }
+  const restorePagesButton = createTag('button', {
+    type: 'button',
+    class: 'restore-btn',
+  }, 'Restore Page Versions');
+  restorePagesButton.addEventListener('click', async () => {
+    const selectedCards = document.querySelectorAll('div.card-body input[type="checkbox"]:checked');
+    const restorePromises = Array.from(selectedCards).map(async (input) => {
+      const card = input.closest('.card-body');
+      const versionUrl = card.querySelector('div.version-lists select')?.value;
+      await restoreVersion(input.value, versionUrl);
+      card.classList.add('modified');
     });
-
-    buttonContainer.append(selectAllButton, deselectAllButton, restorePagesButton);
-    resultHeader.append(resultHeaderText, buttonContainer);
-    return resultHeader;
+    await Promise.all(restorePromises);
+  });
+  buttonContainer.append(selectAllButton, deselectAllButton, restorePagesButton);
+  resultHeader.append(resultHeaderText, buttonContainer);
+  return resultHeader;
 }
 
 window.addEventListener('message', (event) => {
@@ -235,7 +226,7 @@ const callback = async (item, pagePaths) => {
   // Skip if not a document
   if (!item.path.endsWith('.html')) return;
   pagePaths.push(item.path);
-}
+};
 
 async function init() {
   const sdk = await DA_SDK;
@@ -245,7 +236,7 @@ async function init() {
 
   constructPageViewer();
 
-  const openExplorerBtn = document.getElementById('openPageExplorer')
+  const openExplorerBtn = document.getElementById('openPageExplorer');
   openExplorerBtn.addEventListener('click', () => {
     document.querySelector('#modal').showModal();
   });
@@ -261,7 +252,6 @@ async function init() {
   window.addEventListener('click', (e) => {
     if (e.target === modal) modal.style.display = 'none';
   });
-
 
   const submitButton = document.querySelector('[name="submitRestore"]');
 
@@ -281,18 +271,21 @@ async function init() {
 
     const resultHeader = createResultHeader();
 
-    const resultsList = createTag('div', {class: 'results-list'});
-    for (let i = 0; i < pagePaths.length; i++) {
-      const pagePath = pagePaths[i];
-      const result = await getVersionList(pagePath, token);
-      let pageCard;
-      if (result == undefined || result.data.length == 0) {
-        pageCard = createInvalidCard(pagePath);
-      } else {
-        pageCard = createCard(pagePath, result);
+    const resultsList = createTag('div', { class: 'results-list' });
+
+    // Create an array of promises that each return a card
+    const cardPromises = pagePaths.map(async (pagePath) => {
+      const result = await getVersionList(pagePath);
+      if (!result || !result.data.length) {
+        return createInvalidCard(pagePath);
       }
-      resultsList.append(pageCard);
-    }
+      return createCard(pagePath, result);
+    });
+
+    // Wait for all to resolve then append
+    const cards = await Promise.all(cardPromises);
+    cards.forEach((card) => resultsList.append(card));
+
     resultContainer.innerHTML = '';
     resultContainer.append(resultHeader, resultsList);
   });
