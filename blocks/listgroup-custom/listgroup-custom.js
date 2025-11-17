2dccc114492430e41e3f99cd3a794d8af52457be
Laurel Timko <laurel.timko@jmp.com>
2025-11-17 17:34:22 -0500

    change dropdown value not text

--- blocks/listgroup-custom/listgroup-custom.js
+++ blocks/listgroup-custom/listgroup-custom.js
@@ -308,13 +308,14 @@ async function constructDropdown(dictionary, filterBy, defaultFilterOption, tran
 
   sortedList.forEach((filterValue) => {
     console.log(filterValue);
+    let filterString = filterValue;
     const includesTagProperty = checkForTagProperties([filterBy]);
     if (includesTagProperty && window.tagtranslations) {
-      filterValue = window.tagtranslations[filterValue] ? window.tagtranslations[filterValue] : filterValue;
+      filterString = window.tagtranslations[filterValue] ? window.tagtranslations[filterValue] : filterValue;
     }
     console.log(filterValue);
     if (filterValue.length > 0) {
-      const dropdownItem = createTag('option', { value: `${filterValue}` });
+      const dropdownItem = createTag('option', { value: `${filterString}` });
       if (useTranslation) {
         if (useTranslation[filterValue]) {
           dropdownItem.innerText = useTranslation[filterValue];
