e4618d7ec0afee84b3bc23f77c9d0538d45055b9
Laurel Timko <laurel.timko@jmp.com>
2026-02-18 16:37:05 -0500

    add more logging

--- scripts/update-configs.mjs
+++ scripts/update-configs.mjs
@@ -1,12 +1,10 @@
 import fs from 'node:fs/promises';
 
-export default async function sendPostRequest(authToken, configPath, configType) {
+export default async function sendPostRequest(authToken, yamlText, configType) {
   const url = `https://admin.hlx.page/config/jmphlx/sites/jmp-da/content/${configType}`;
 
+  console.log('pre posssttt');
   try {
-    const yamlText = await fs.readFile(configPath, 'utf8');
-    console.log(yamlText);
-
     const response = await fetch(url, {
       method: 'POST',
       headers: {
@@ -35,6 +33,12 @@ const authToken = process.env.AUTH_TOKEN;
 const configPath = process.env.CONFIG_PATH;
 const configName = process.env.CONFIG_NAME;
 
-const result = await sendPostRequest(authToken, configPath, configName);
+console.log('prePOST');
+
+const yamlText = await fs.readFile(configPath, 'utf8');
+console.log('yaml');
+console.log(yamlText);
+
+const result = await sendPostRequest(authToken, yamlText, configName);
 console.log('result here');
 console.log(result);
