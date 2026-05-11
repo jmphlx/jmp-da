e1c794f15fc5fc00aeb9076ae418e86daaceac0f
Laurel Timko <laurel.timko@jmp.com>
2026-05-11 10:23:26 -0400

    Undo return statement

--- blocks/header/search.js
+++ blocks/header/search.js
@@ -50,5 +50,5 @@ async function populateSearchResults(searchTerms, resultsContainer) {
 }
 
 export function onSearchInput(value, resultsContainer) {
-  return populateSearchResults(value, resultsContainer);
+  populateSearchResults(value, resultsContainer);
 }
