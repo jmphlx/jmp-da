640e2e6242c18052dd77b13ca23a2410bf217661
Laurel Timko <laurel.timko@jmp.com>
2026-07-31 14:45:53 -0400

    Try token instead of bearer

--- scripts/post-event-processing.mjs
+++ scripts/post-event-processing.mjs
@@ -68,7 +68,7 @@ async function updatePastEventPage(authToken, page) {
     const response = await fetch(url, {
       method: 'GET', 
       headers: {
-        'Authorization': `Bearer ${authToken}` ,
+        'Authorization': `token ${authToken}` ,
         'Accept': '*/*'
       }
     });
